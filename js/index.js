203336b67049edd77f8ba2d70839dd1747ee3a83
khlevnov <olegkhlevnov@gmail.com>
2016-05-27 00:52:28 +0300

    Включил разные эффекты для свайпера в зависомости от устройства

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 import Swiper from 'swiper';
+import MenuCatalog from './blocks/MenuCatalog.js';
 
-import './blocks/MenuCatalog.js';
+var menu = new MenuCatalog;
 
 var mainpageSlider = new Swiper('.slider', {
     autoplay: 3000,
+    autoplayDisableOnInteraction: false,
+    effect: document.documentElement.clientWidth > 960 ? 'fade' : 'slide',
+    lazyLoading: true,
     loop: true,
     pagination: '.swiper-pagination',
     paginationClickable: true,
